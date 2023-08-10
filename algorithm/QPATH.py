b51c86d0367deaecbe1ed9342b90d9b85705a9ca
winston <winstonyi02@gmail.com>
2023-08-09 18:18:01 -0700

    Issue with kHopNeighbors identifying neighbors that there are no links to. Creates an infintie while. Added sort_link to allow correct indexiing when referencing purifaction table and purification decisions. Removed entanglement swap calculations since we just multiply. Removed all changes in purification table and calculations now depend on pur table and pur decisions instead of just pur table. Added method to check memory constraints. Changed path data structure in MinHeap to route to help distinguish [node, node, node] path from [cost, path, D_pur]. Still struggling to find a way to check link constraints

--- algorithm/QPATH.py
+++ algorithm/QPATH.py
@@ -12,13 +12,15 @@ import heapq
 
 # Path tracing credits to https://stackoverflow.com/questions/8922060/how-to-trace-the-path-in-a-breadth-first-search
 
-# Returns the fidelity after an entanglement swapping
-def swap_fidelity(f):
-    return f**2 + ((1-f)**2)/3
-
 # Returns the fidelity after a single purification
 def purify(f1, f2):
     return f1*f2/(f1*f2 + (1-f1)*(1-f2))
+
+def sort_link(n1, n2):
+    if n1.id > n2.id:
+        return (n2, n1)
+    return (n1, n2)
+
 
 # Removes all links that have max fidelity < required threshold
 def remove_lower_threshold(purification_table, threshold):
@@ -53,38 +55,47 @@ class QPath():
         if shortest_route_length == -1:
             return
         update_graph = self.topo
-        D_pur = defaultdict(lambda: 0)
 
         # for Hmin: E|C|:
-        for min_cost in range(shortest_route_length, len(self.purification_table.keys())*max([len(k) for k in self.purification_table.values()])):
+        # for min_cost in range(shortest_route_length, len(self.purification_table.keys())*max([len(k) for k in self.purification_table.values()])):
+        for min_cost in range(4):
             paths = self.k_shortest_paths(source, dst, reqs)
             for path in paths:
+                print(path)
                 cost = 0
-                path_fidelity = self.calc_path_fidelity(path)
+                D_pur = defaultdict(lambda: 0)
+                path_fidelity = self.calc_path_fidelity(path, D_pur)
                 while path_fidelity < self.threshold:
-                    link = self.min_fidelity_link(path) # Identify link with minimum fidelity to purify
+                    link = self.min_fidelity_link(path, D_pur) # Identify link with minimum fidelity to purify
+                    if link[0] == link[1]: # No possible purifications
+                        break
                     D_pur[link] += 1
                     cost += 1
-                    self.purification_table[link] = self.purification_table[link][1:]
-                    path_fidelity = self.calc_path_fidelity(path) # Since calc path fid is based off table, updates in table affect fidelity
+                    path_fidelity = self.calc_path_fidelity(path, D_pur) 
+                    break
                 cost += len(path) - 1 # Num entanglements used for swapping
                 pq.push(cost, path, D_pur)
-            # path = pq.pop()
-            # while path[0] <= min_cost + 1:
-            #     if self.path_width(path, D_pur) >= 1:
-                    # for edge in path:
-                    #   edge - min(path_width, Reqs) - num_purification on the edge (from D_pur)
-            #     path = pq.pop()
+            # route = pq.pop() # (cost, path, D_pur)
+            # while pq.get_length() > 0 and route[0] <= min_cost + 1:
+            #     path_width = self.calc_path_width(route)
+            #     if path_width >= 1:
+            #         for link in route[1]:
+            #             print(link)
+            #             # has_capacity(edge, cap) and has_memory(edge, mem)
+            #             # link - min(path_width, reqs) - num_purification on the edge (from D_pur)
+            #     route = pq.pop()
         return pq
 
-
-
-    def path_width(self, path, D_pur):
+    def has_memory(link, mem):
+        # T/F if link nodes have nQubits
+        return min(link.n1.nQubits, link.n2.nQubits) >= mem
+    
+    def calc_path_width(self, route):
         # Finds Wmin(i, j) defined in pg7
         min_width = 0
-        n1 = n2 = path[0]
-        for i in range(len(path) - 1):
-            min_width = min(min_width, len(self.purification_table[(path[i], path[i+1])])/(D_pur[n1, n2] + 1))
+        n1 = n2 = route[0]
+        for i in range(len(route) - 1):
+            min_width = min(min_width, len(self.purification_table[(route[i], route[i+1])])/(route[2][n1, n2] + 1))
         return min_width
         
                 
@@ -105,25 +116,28 @@ class QPath():
                     dist[link.n2] = min(dist[source] + 1, dist[link.n2])
         return -1
     
-    def calc_path_fidelity(self, path):
-        # Calculates fid through 0 purification links in pur table
+    def calc_path_fidelity(self, path, D_pur):
         fidelity = 1
         for i in range(len(path) - 1):
-            fidelity = fidelity * self.purification_table[(path[i], path[i+1])][0]
+            link = sort_link(path[i], path[i+1])
+            fidelity = fidelity * self.purification_table[link][D_pur[link]]
         return fidelity
                 
 
-    def min_fidelity_link(self, path):
+    def min_fidelity_link(self, path, D_pur):
         # Find the link with the minimum fidelity through the purification table
         min_fid = 1
         n1 = n2 = path[0]
         for i in range(len(path) - 1):
-            if self.purification_table[(path[i], path[i+1])][0] < min_fid:
-                min_fid = self.purification_table[(path[i], path[i+1])][0]
+            link = sort_link(path[i], path[i+1])
+            if D_pur[link] >= len(self.purification_table[link]) - 1: # No more possible purifications on link
+                continue
+            link_fid = self.purification_table[link][D_pur[link]]
+            if link_fid < min_fid:
+                min_fid = link_fid
                 n1, n2 = path[i], path[i+1]
         return (n1, n2) # Doesn't actually return link object, just two nodes
     
-    # Do I need shortest path with <= k hops or k shortest paths???
     def k_shortest_paths(self, src, dst, k):
         def dijkstra(src): # Dijkstra's makes this an offline algorithm
             dist = {node: float('inf') for node in self.topo.nodes}
@@ -132,11 +146,11 @@ class QPath():
             heap.push(0, src.id, src) # Will break if heap evaluates objects, so use src.id to stop all comparison ties
 
             while heap.get_length() > 0:
-                curr_dist, curr_node_id, curr_node = heap.pop()
+                curr_dist, _, curr_node = heap.pop()
 
                 if curr_dist > dist[curr_node]:
                     continue
-    
+
                 for neighbor in self.topo.kHopNeighbors(curr_node, 1):
                     distance = curr_dist + 1
                     if distance < dist[neighbor]:
@@ -153,7 +167,7 @@ class QPath():
         i = 0
         heap.push(0, i, [src])
         while heap.get_length() > 0 and len(paths) < k:
-            curr_dist, iter, curr_path = heap.pop()
+            curr_dist, _, curr_path = heap.pop()
             curr_node = curr_path[-1]
 
             if curr_node == dst:
--- dev.py
+++ dev.py
@@ -1,28 +1,23 @@
-# from topo.Topo import Topo
-# from algorithm.QPATH import QPath
+from topo.Topo import Topo
+from algorithm.QPATH import QPath
 
-# netTopology = Topo.generateString(5, 0.6, 5, 0.1, 6)
-# topo = Topo(netTopology)
+netTopology = Topo.generateString(5, 0.6, 5, 0.1, 6)
+topo = Topo(netTopology)
 
-# algo = QPath(topo, 0.95)
+from collections import Counter
+l = []
+for link in topo.links:
+    l.append((link.n1, link.n2))
+d = Counter(l)
+print(d)
 
-# from collections import Counter
-# l = []
-# for link in topo.links:
-#     l.append((link.n1, link.n2))
-# d = Counter(l)
 
-# res =  algo.P2(topo.nodes[0], topo.nodes[3], 1) 
+algo = QPath(topo, 0.95)
+
+
+print(algo.P2(topo.nodes[0], topo.nodes[3], 2))
 # print(res)
 # print(d[topo.nodes[0], topo.nodes[3]])
 
 # print(algo.P2(topo.nodes[0], topo.nodes[3], 1))
-
-from collections import Counter, defaultdict
-
-
-d = defaultdict(lambda: 0)
-print(d['hey'])
-d['hey'] += 1
-print(d['hey'])
 
