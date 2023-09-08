8a1c740adc740edc55d305873c0da4a74247f7e7
winston <winstonyi02@gmail.com>
2023-09-07 18:48:19 -0700

    Extended dijkstra implemented and tested. Determined average fidelity, path of distance by fidelity found by extended shortest path first. Sets/other methods on classes can be implemented if we make a big change and change two dunder methods for link cmp/eq and hash. Also, although path lengths are the same (and thus the cost when the fidelity of every edge is equal) sometimes the library k-shortest provides a path of same length but different nodes than the espf algo. Unsure why, possibly different ordering when adding links to the graph in Topo.py?

--- dev.py
+++ dev.py
@@ -2,23 +2,12 @@ from topo.Topo import Topo
 from algorithm.QLEAP import QLeap
 from collections import Counter
 import matplotlib.pyplot as plt
+import random
 
-# netTopology = Topo.generateString(5, 0.6, 5, 0.1, 6)
-# topo = Topo(netTopology)
-# algo = QLeap(topo, 0.6)
+n = 300
 
-# print(algo.P2(topo.nodes[1], topo.nodes[0], 1))
+netTopology = Topo.generateString(n, 0.6, 5, 0.1, 6)
+topo = Topo(netTopology)
+algo = QLeap(topo, 0.6)
 
-class Test():
-    def __init__(self, n1, n2):
-        self.n1 = min(n1, n2)
-        self.n2 = max(n1, n2)
-    def __eq__(self, other):
-        if self.n1 == other.n1 and self.n2 == other.n2:
-            return True
-        return False
-    def __hash__(self):
-        return hash((self.n1, self.n2))
-l = [Test(1, 2), Test(1, 2)]
-s = set(l)
-print(s)
+print(algo.P2(topo.nodes[random.randint(0, n-1)], topo.nodes[random.randint(0, n-1)], 1))
